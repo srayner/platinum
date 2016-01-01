09c3919c1136b37b54621ab2c55d57561fed3cd5
Steve Rayner <srayner02@gmail.com>
2016-01-01 20:10:19 +0000

    Stopped tracking vendor folder.

--- vendor/bin/doctrine-module.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd .
-cd %~dp0
-cd "../doctrine/doctrine-module/bin"
-set BIN_TARGET=%CD%\doctrine-module
-popd
-php "%BIN_TARGET%" %*
--- vendor/bin/doctrine.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd .
-cd %~dp0
-cd "../doctrine/orm/bin"
-set BIN_TARGET=%CD%\doctrine
-popd
-php "%BIN_TARGET%" %*
