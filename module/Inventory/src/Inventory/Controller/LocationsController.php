92825a6d7479ba27df6460a167fcb4a924d5cfdd
Steve Rayner <srayner02@gmail.com>
2016-01-03 11:44:36 +0000

    Fixed bug with deleting a location.

--- module/Inventory/src/Inventory/Controller/LocationsController.php
+++ module/Inventory/src/Inventory/Controller/LocationsController.php
@@ -11,74 +11,74 @@ use Zend\Mvc\Controller\AbstractActionController,
 
 class LocationsController extends AbstractInventoryController
 {	
-	public function indexAction()
+    public function indexAction()
+    {
+        $aColumns = array('l.location_code', 'l.location_name');
+		
+	// Get parameters from the ajax request
+	$sEcho          = $this->params()->fromQuery('sEcho');
+	$iDisplayStart  = $this->params()->fromQuery('iDisplayStart', 0);
+	$iDisplayLength = $this->params()->fromQuery('iDisplayLength', 10);
+	$iSortingCols   = $this->params()->fromQuery('iSortingCols', 0);
+	$iSortIndex     = $this->params()->fromQuery('iSortCol_0', 0);
+	$sSortDir       = $this->params()->fromQuery('sSortDir_0', 'asc');
+	$sSearch        = $this->params()->fromQuery('sSearch', '');
+		
+	// Build the from clause.
+	$from = 'FROM Inventory\Entity\Location l ';
+		
+	// Get record count.
+	$dql = 'SELECT COUNT(l.id) ' . $from;
+	$where = '';
+	if($sSearch != '')
 	{
-		$aColumns = array('l.location_code', 'l.location_name');
-		
-		// Get parameters from the ajax request
-		$sEcho          = $this->params()->fromQuery('sEcho');
-		$iDisplayStart  = $this->params()->fromQuery('iDisplayStart', 0);
-		$iDisplayLength = $this->params()->fromQuery('iDisplayLength', 10);
-		$iSortingCols   = $this->params()->fromQuery('iSortingCols', 0);
-		$iSortIndex     = $this->params()->fromQuery('iSortCol_0', 0);
-		$sSortDir       = $this->params()->fromQuery('sSortDir_0', 'asc');
-		$sSearch        = $this->params()->fromQuery('sSearch', '');
-		
-		// Build the from clause.
-		$from = 'FROM Inventory\Entity\Location l ';
-		
-		// Get record count.
-		$dql = 'SELECT COUNT(l.id) ' . $from;
-		$where = '';
-		if($sSearch != '')
-		{
-			$where = "WHERE l.location_code LIKE '%" . $sSearch . "%' OR " . 
-					 "l.location_name LIKE '%" . $sSearch . "%' ";
-			$dql = $dql . $where;
-		}
-		$query = $this->getEntityManager()->createQuery($dql);
-		$count = $query->getResult(Query::HYDRATE_SINGLE_SCALAR);
-		
-		// Build the order by clause.
-		$orderBy = '';
-		if ($iSortingCols > 0)
-		{
-			$orderBy = 'ORDER BY ' . $aColumns[$iSortIndex] . ' ' . $sSortDir . ' ';
-		}
-		
-		// build query
-		$dql = 'SELECT l ' . $from . $where  . $orderBy;
-		$query = $this->getEntityManager()->createQuery($dql);
-		$query->setFirstResult($iDisplayStart);
-		$query->setMaxResults($iDisplayLength);
-		
-		// Build the view models, passing in all relevant data.
-		$variables = array(
-				"sEcho" => $sEcho,
-				"iTotalRecords" => $count,
-				"iTotalDisplayRecords" => $count,
-				'locations' => $query->getResult()
-		);
-		if ($this->flashMessenger()->hasMessages())
-		{
-			$variables['messages'] = $this->flashMessenger()->getMessages();
-		}
-		$view = new ViewModel($variables);
-		
-		// Make adjustments if request is expecting JSON response.
-		if ($this->request->isXmlHttpRequest())
-		{
-			$view->setTerminal(true);
-			$view->setTemplate('inventory/locations/data');
-		}
-		
-		// Finally return the view
-		return $view;
-	    
+	    $where = "WHERE l.location_code LIKE '%" . $sSearch . "%' OR " . 
+                     "l.location_name LIKE '%" . $sSearch . "%' ";
+	    $dql = $dql . $where;
 	}
-	
-	public function addAction()
+	$query = $this->getEntityManager()->createQuery($dql);
+	$count = $query->getResult(Query::HYDRATE_SINGLE_SCALAR);
+		
+	// Build the order by clause.
+	$orderBy = '';
+	if ($iSortingCols > 0)
 	{
+	    $orderBy = 'ORDER BY ' . $aColumns[$iSortIndex] . ' ' . $sSortDir . ' ';
+	}
+		
+	// build query
+	$dql = 'SELECT l ' . $from . $where  . $orderBy;
+	$query = $this->getEntityManager()->createQuery($dql);
+	$query->setFirstResult($iDisplayStart);
+	$query->setMaxResults($iDisplayLength);
+		
+	// Build the view models, passing in all relevant data.
+	$variables = array(
+	    "sEcho" => $sEcho,
+	    "iTotalRecords" => $count,
+            "iTotalDisplayRecords" => $count,
+	    'locations' => $query->getResult()
+	);
+	if ($this->flashMessenger()->hasMessages())
+	{
+	    $variables['messages'] = $this->flashMessenger()->getMessages();
+	}
+	$view = new ViewModel($variables);
+		
+	// Make adjustments if request is expecting JSON response.
+	if ($this->request->isXmlHttpRequest())
+	{
+	    $view->setTerminal(true);
+	    $view->setTemplate('inventory/locations/data');
+	}
+		
+	// Finally return the view
+	return $view;
+	    
+    }
+	
+    public function addAction()
+    {
 		// Create a new form instance.
 		$form = new LocationForm();
 		$form->get('submit')->setAttribute('label', 'Add');
@@ -101,7 +101,7 @@ class LocationsController extends AbstractInventoryController
 				$this->getEntityManager()->flush();
 	
 				// Create information message.
-				$this->flashMessenger()->addMessage('Location ' . $location->location_code . ' sucesfully added');
+				$this->flashMessenger()->addMessage('Location ' . $location->location_code . ' succesfully added');
 				
 				// Redirect to list of locations
 				return $this->redirect()->toRoute('inventory/default', array('controller' => 'locations'));
@@ -110,10 +110,10 @@ class LocationsController extends AbstractInventoryController
 	
 		// If not a POST request, then just render the form.
 		return array('form' => $form);
-	}
-	
-	public function editAction()
-	{
+    }
+    
+    public function editAction()
+    {
 	    // Get a current copy of the entity.
 		$id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
 		if (!$id) {
@@ -151,48 +151,47 @@ class LocationsController extends AbstractInventoryController
 				'id' => $id,
 				'form' => $form,
 		);
+    }
+
+    public function deleteAction()
+    {
+	$id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
+	if (!$id) {
+            return $this->redirect()->toRoute('inventory/default', array('controller' => 'locations'));
 	}
 	
-	public function deleteAction()
-	{
-		$id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
-		if (!$id) {
-			return $this->redirect()->toRoute('inventory/default', array('controller' => 'locations'));
-		}
-	
-		// Create a new form instance.
-		$form = new ConfirmationForm();
+	// Create a new form instance.
+	$form = new ConfirmationForm();
 		
-		$request = $this->getRequest();
-		if ($request->isPost()) {
-			$del = $request->getPost()->get('yes', 'No');
-			if ($del == 'Yes') {
-				$id = (int)$request->getPost()->get('id');
-				$location = $this->getEntityManager()->find('Inventory\Entity\Location', $id);
-				if ($location)
-				{    
-				    // Delete from the database.
-					$this->getEntityManager()->remove($location);
-					$this->getEntityManager()->flush();
-					
-					// Create information message.
-					$this->flashMessenger()->addMessage('Location ' . $location->location_code . ' sucesfully deleted');
-				}
-			}
-	
-			// Redirect to list of locations
-			return $this->redirect()->toRoute('inventory/default', array(
-					'controller' => 'locations',
-					'action' => 'index',
-			));
-		}
-	
-		$form->populateValues(array('id' => $id));
-		return array(
-				'id' => $id,
-				'location' => $this->getEntityManager()->find('Inventory\Entity\Location', $id)->toArray(),
-		        'form' => $form
-		);
+	$request = $this->getRequest();
+	if ($request->isPost()) {
+            $del = $request->getPost()->get('yes', 'No');
+            if ($del == 'Yes') {
+                $id = (int)$request->getPost()->get('id');
+                $location = $this->getEntityManager()->find('Inventory\Entity\Location', $id);
+                if ($location)
+                {
+                    // Delete from the database.
+                    $this->getEntityManager()->remove($location);
+                    $this->getEntityManager()->flush();
+
+                    // Create information message.
+                    $this->flashMessenger()->addMessage('Location ' . $location->location_code . ' sucesfully deleted');
+                }
+            }
+
+            // Redirect to list of location.
+            return $this->redirect()->toRoute('inventory/default', array(
+                'controller' => 'locations',
+		'action' => 'index',
+            ));
 	}
+	
+	$form->populateValues(array('id' => $id));
+	return array(
+            'location' => $this->getEntityManager()->find('Inventory\Entity\Location', $id),
+            'form' => $form
+	);
+    }
 	
 }
