989b6a98886d82f87326fb15758f098522e4d67d
Steve Rayner <srayner02@gmail.com>
2016-04-17 22:14:38 +0100

    Added purchasing module configuration.

---
+++ module/Purchasing/config/module.config.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Purchasing;
+
+return array(
+    
+    // Router configuration.
+    'router' => array(
+        'routes' => array(
+            'purchasing' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/purchasing',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Purchasing\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action][/:id]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                                    
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+	),
+    ),
+    
+    // Controller configurtion.
+    'controllers' => array(
+        'invokables' => array(
+            'Purchasing\Controller\Index'     => 'Purchasing\Controller\IndexController',
+            'Purchasing\Controller\Account'   => 'Purchasing\Controller\AccountController',
+            'Purchasing\Controller\Order'     => 'Purchasing\Controller\OrderController',
+        ),
+    ),
+
+    // View manager configuration.
+    'view_manager' => array(
+        'template_map' => include __DIR__  .'/../template_map.php',
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    
+    // Service manager
+    'service_manager' => array(
+        'invokables' => array(
+            'purchasing_account'    => 'Purchasing\Entity\Account',
+            'purchasing_order'      => 'Purchasing\Entity\Order',
+            'purchasing_order_line' => 'Purchasing\Entity\OrderLine',
+        ),
+        'factories' => array(
+            'purchasing_account_form'    => 'Purchasing\Form\AccountFormFactory',
+            'purchasing_order_form'      => 'Purchasing\Form\OrderFormFactory',
+            'purchasing_order_line_form' => 'Purchasing\Form\LineFormFactory',
+        ),
+    ),
+    
+    // Doctrine configuration.
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'
+                ),
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ),
+            ),
+        ),
+    ),
+    
+);
